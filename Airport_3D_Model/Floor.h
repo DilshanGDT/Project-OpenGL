a6871b56892c4e0278fc4dacf1de19ff3fb4fdd2
Dilshan <tharakadilshan506@gmail.com>
2025-01-29 23:07:38 +0530

    create new floor classes

---
+++ Airport_3D_Model/Floor.h
@@ -0,0 +1,20 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+
+#include <GL/glut.h>
+
+// Declare the texture array globally
+// extern GLuint pierTextures[6];
+
+// Declare texture-loading function
+// void loadPierTextures();
+
+// Declare the pier functions
+void mainFloor_airport();
+void roadSegments();
+void roads();
+void grassSegment();
+void grass();
+void airportFloor();
+
+#endif
